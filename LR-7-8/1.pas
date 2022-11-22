ee73c3ce59cc3f1d3ef04837a5e22da6e6726af1
prismatichniy <117262805+prismatichniy@users.noreply.github.com>
2022-11-22 14:21:18 +0300

    Add files via upload

---
+++ LR-7-8/1.pas
@@ -0,0 +1,20 @@
+﻿var
+a,b,c: string;
+i,f,g: byte;
+begin 
+
+
+write('Исходная строка: '); 
+readln(a);
+b:='Genadii';
+i:= length(b);
+c:= 'Alexandr';
+while pos(b,a)>0 do
+ begin
+  f:=pos(b,a);
+  
+  delete(a,f,7);
+  insert(c,a,f);
+ end;
+ write(a);
+end.
